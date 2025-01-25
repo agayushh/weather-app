aa8ef9227ff155f3ea208415aee5740c7567fcd7
AayushhGoyal <agayush088@gmail.com>
2025-01-25 17:17:30 +0530

    added settings page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 // import Home from "./pages/Home";
 // import Dashboard from "./pages/Dashboard";
 // import Cities from "./pages/Cities";
-import Maps from "./pages/Maps";
+// import Maps from "./pages/Maps";
+import Settings from "./pages/Settings";
 
 
 export default function App() {
 
   return <div>
     {/* <Dashboard />` */}
-    <Maps />
-
+    {/* <Maps /> */}
+  <Settings />
     {/* <Cities /> */}
     {/* <Home /> */}
   </div>;
