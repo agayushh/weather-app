fe05f6c6a4bb18252f5424f74d49f22b84080e02
AayushhGoyal <agayush088@gmail.com>
2025-01-25 17:16:06 +0530

    added maps page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // import Home from "./pages/Home";
 // import Dashboard from "./pages/Dashboard";
-import Cities from "./pages/Cities";
+// import Cities from "./pages/Cities";
+import Maps from "./pages/Maps";
 
 
 export default function App() {
 
   return <div>
     {/* <Dashboard />` */}
+    <Maps />
 
-    <Cities />
+    {/* <Cities /> */}
     {/* <Home /> */}
   </div>;
 }
---
+++ src/pages/Maps.jsx
@@ -0,0 +1,19 @@
+export default function Maps() {
+  return <div>
+     <div className="bg-[#0b131e] h-[100vh] w-full">
+      <div className="flex grow">
+        <div className="flex items-center h-[100vh] ml-10">
+          <div className="bg-[#202b3b] h-[95%] w-24 rounded-2xl">Maps</div>
+        </div>
+
+        <div className="mt-8">
+          <input
+            type="text"
+            placeholder="Search For Cities"
+            className="bg-[#202b3b] w-[900px] ml-12 p-4 rounded-2xl text-white placeholder:text-slate-200 hover:border hover:border-sky-300"
+          />
+        </div>   
+      </div>
+    </div>
+  </div>;
+}
