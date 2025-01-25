e6a594a9f77dd5f68f618e2cd713f3547da3c32f
AayushhGoyal <agayush088@gmail.com>
2025-01-25 15:10:50 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-[#0b131e] h-[100vh] w-full flex items-center flex-wrap">
+      <div className="bg-[#202b3b] h-[95%] w-[45%] ml-16 rounded-3xl"></div>
+    </div>
+  );
+}
