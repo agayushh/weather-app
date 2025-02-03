f82c1ec4c41c4398e1a2036635fc3bbd473dd9be
AayushhGoyal <agayush088@gmail.com>
2025-02-03 11:35:08 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-// import Home from "./pages/Home";
-// import Dashboard from "./pages/Dashboard";
-// import Cities from "./pages/Cities";
-// import Maps from "./pages/Maps";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Cities from "./pages/Cities";
+import Maps from "./pages/Maps";
 import Settings from "./pages/Settings";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
-
-  return <div>
-    {/* <Dashboard /> */}
-    {/* <Maps /> */}
-  <Settings />
-    {/* <Cities /> */}
-    {/* <Home /> */}
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/maps" element={<Maps />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/cities" element={<Cities />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
