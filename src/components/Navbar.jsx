2e4dbd0a0c5159543ed3fa736857d5eb801a9f2b
AayushhGoyal <agayush088@gmail.com>
2025-02-03 11:39:14 +0530

    added routes to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,29 @@ import umbrella from "../assets/umbrella.png";
 import { TiWeatherCloudy } from "react-icons/ti";
 import { FaCity } from "react-icons/fa";
 import { IoIosSettings } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const navbarItems = [
     {
       name: "Weather",
       Icon: TiWeatherCloudy,
+      path: "/dashboard",
     },
     {
       name: "Cities",
       Icon: FaCity,
+      path: "/cities",
     },
     {
       name: "Maps",
       Icon: IoMapSharp,
+      path: "/maps",
     },
     {
       name: "Settings",
       Icon: IoIosSettings,
+      path: "/settings",
     },
   ];
   return (
@@ -30,14 +35,15 @@ export default function Navbar() {
           <div className="bg-[#202b3b] h-[95%] w-24 rounded-2xl p-2 flex justify-center ">
             <div className="flex flex-col items-center gap-y-7">
               <img src={umbrella} alt="" className="mb-3" />
-              {navbarItems.map(({ Icon, name }, index) => (
-                <div
+              {navbarItems.map(({ Icon, name, path }, index) => (
+                <Link
+                  to={path}
                   className="flex flex-col gap-1 items-center justify-center"
                   key={index}
                 >
                   <Icon className="text-4xl text-white hover:scale-125 duration-500" />
                   <p className="text-white text-sm">{name}</p>
-                </div>
+                </Link>
               ))}
             </div>
           </div>
